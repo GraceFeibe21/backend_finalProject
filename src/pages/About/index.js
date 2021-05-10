eb4083eb7403ff99cabbe0246a695da06872bbcb
GraceFeibe21 <s11810232@student.unklab.ac.id>
2021-05-10 10:15:34 +0800

    commit About

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,9 +3,12 @@ import NavBar from '../../components/molecules/NavBar'
 
 const About = () => {
     return (
-        <div>
+        <div className="container">
             <NavBar /> 
+            <div className="container">
             <h3>About</h3>
+            <p>Dibuat oleh Grace Feibe Pikirang</p>
+            </div>
         </div>
     )
 }
